7e2110bc4e0cc8ee2e13906d95379b193eb3c7a3
NHGamer <aniket94sharma56@gmail.com>
2026-09-07 06:24:28 +0530

    add album and song controllers with routes

--- backend/src/routes/album.routes.js
+++ backend/src/routes/album.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAlbums, getAllAlbums } from "../controller/album.controller.js";
 const albumRouter= Router()
 
+albumRouter.get("/", getAllAlbums);
+albumRouter.get("/:albumId", getAlbums);
 
-export default albumRouter
+export default albumRouter;
--- backend/src/routes/song.routes.js
+++ backend/src/routes/song.routes.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import { getAllSongs, getFeaturedSongs, getMadeForYouSongs, getTrendingSongs } from "../controller/songs.controller.js";
 const songRouter=Router()
-
-
+ 
+songRouter.get("/",protectRoute, requireAdmin, getAllSongs);
+songRouter.get("/featured", getFeaturedSongs);
+songRouter.get("/made-for-you", getMadeForYouSongs);
+songRouter.get("/trending", getTrendingSongs);
+ 
 export default songRouter
